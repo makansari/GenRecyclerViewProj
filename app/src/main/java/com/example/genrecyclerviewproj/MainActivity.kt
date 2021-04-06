670ae318e2c7a77c1b0fe19db7fa5edcc3d41396
Pallavitomarltts <78848661+Pallavitomarltts@users.noreply.github.com>
2021-04-06 17:17:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/genrecyclerviewproj/MainActivity.kt
+++ app/src/main/java/com/example/genrecyclerviewproj/MainActivity.kt
@@ -17,12 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         var myDatalist = ArrayList<UsersData>()
 
-        myDatalist.add(UsersData("ansari","a@gamil.com","945484444"))
-        myDatalist.add(UsersData("abhishek","abhishek@gamil.com","345345345"))
-        myDatalist.add(UsersData("rahul","rahul@gamil.com","9454834543"))
-        myDatalist.add(UsersData("vijay","vijay@gamil.com","777644545"))
-        myDatalist.add(UsersData("abdul","a@gamil.com","777644545"))
-
+        myDatalist.add(UsersData("pallavi","pallavi@gamil.com","9876543210"))
+        myDatalist.add(UsersData("harshit","harshu@gamil.com","9345345345"))
+        myDatalist.add(UsersData("sirisha","sirisha@gamil.com","9499834543"))
+        myDatalist.add(UsersData("supriya","supriya@gamil.com","89894545"))
+        myDatalist.add(UsersData("shrishti","shrishti@gamil.com","7987644545"))
+        myDatalist.add(UsersData("vishal","vishal@gamil.com","712345945"))
             myrv.adapter = MyAdapter(myDatalist)
 
     }
